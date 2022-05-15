46541a4a74d7ee08ccbe5f525f28c7c160bc28fb
aalaa-hesham <alaahesham408@gmail.com>
2022-05-15 22:39:48 +0200

    task2

--- task 1/root.php
+++ task 1/root.php
@@ -6,22 +6,7 @@ if($_POST){
     $power = $_POST ['power'];
     $sqrt = ($num ** (1/$power));
 }
-
-
-
 ?>
-
-
-
-
-
-
-
-
-
-
-
-
 
 <!DOCTYPE html> 
 <html lang="en">
@@ -54,14 +39,10 @@ if($_POST){
 <button class=" btn btn-outline-danger form-control text-center mt-3"> ROOT </button>
 </div>
 <?php echo $sqrt; ?>
+</div>
 </form>
+</div>        
 </div>
-</div>
-                   
-    
-</div>
-    
-
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
